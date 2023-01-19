209cf1cf4051a3456d40350136fed5e14179d9ae
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-19 23:02:04 +0200

    Homework 4 done

--- src/com/ua/HomeWork/l4/Main.java
+++ src/com/ua/HomeWork/l4/Main.java
@@ -3,12 +3,12 @@ package com.ua.HomeWork.l4;
 public class Main {
 
     public static void main(String[] args) {
-        String str = "CyberSecurity";
+        String str = "";
         int lenght = str.length();
         int LastIndex = lenght - 1;
 
         System.out.println(str.toUpperCase());
         System.out.println(str.toLowerCase());
-        System.out.println((str.substring(0,1) + (str.substring(LastIndex))));
+
     }
 }
---
+++ src/com/ua/HomeWork/l4/MainV2.java
@@ -0,0 +1,16 @@
+package com.ua.HomeWork.l4;
+
+import java.util.Scanner;
+
+public class MainV2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть слово: ");
+        String fromInput = scanner.nextLine();
+        int lenght = fromInput.length();
+        int LastIndex = lenght - 1;
+        System.out.println(fromInput.toLowerCase());
+        System.out.println(fromInput.toUpperCase());
+        System.out.println((fromInput.substring(0, 1) + (fromInput.substring(LastIndex))));
+    }
+}
