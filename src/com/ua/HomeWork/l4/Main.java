0ed62c0f8303f2922629fd42a9bcfa1964ed4014
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-20 16:54:39 +0200

    Homework 4 done

--- src/com/ua/HomeWork/l4/Main.java
+++ src/com/ua/HomeWork/l4/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String str = "Powershell";
         int lenght = str.length();
-        int LastIndex = lenght - 1;
+        int lastIndex = lenght - 1;
         System.out.println(str.toLowerCase());
         System.out.println(str.toUpperCase());
         System.out.println((str.substring(0,1)) + (str.substring(LastIndex)));
