54babea73bd67c1288890014d7ec339110ae1333
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-26 22:02:51 +0200

    Homework 6 done

---
+++ src/com/ua/HomeWork/l6/Fibonacci.java
@@ -0,0 +1,21 @@
+package com.ua.HomeWork.l6;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11 ; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void pairing(String[] args) {
+
+        for (int i = 1; i < 101 ; i++) {
+            if (i %2 == 0){
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
