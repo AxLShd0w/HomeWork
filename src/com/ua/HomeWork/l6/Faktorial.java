187a815e084f6bbedd334dfa90736401f54fbb6f
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-29 08:47:15 +0200

    Homework 6 done

---
+++ src/com/ua/HomeWork/l6/Faktorial.java
@@ -0,0 +1,14 @@
+package com.ua.HomeWork.l6;
+
+public class Faktorial {
+
+    public static void main(String[] args) {
+
+        int fakt = 1;
+        int number = 7;
+        for (int i = 1; i <= number; i++) {
+            fakt = fakt * i;
+        }
+        System.out.println("Faktorlial of "+number+" is: " + fakt);
+    }
+}
