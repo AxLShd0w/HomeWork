56d86577e31ba0af947d5b644abf7e6e0c11db1a
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-01-29 09:24:52 +0200

    Homework 6 done

---
+++ src/com/ua/HomeWork/l6/PairedHundret.java
@@ -0,0 +1,16 @@
+package com.ua.HomeWork.l6;
+
+public class PairedHundret {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+
+
+
