ed3a97b74e7dcebcf94406cc7a00471240ae942d
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-02-11 00:10:59 +0200

    Homework 10 done

--- src/com/ua/HomeWork/l10/Arrays.java
+++ src/com/ua/HomeWork/l10/Arrays.java
@@ -1,4 +1,57 @@
 package com.ua.HomeWork.l10;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Arrays {
+    int[] arr;
+    int arrSize;
+    int maxNumber;
+
+    void arrCreate() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array size : ");
+        arrSize = sc.nextInt();
+        arr = new int[arrSize];
+    }
+
+    void fillArray() {
+        Random random = new Random();
+        for (int i = 0; i < arrSize; i++) {
+            arr[i] = random.nextInt(maxNumber);
+        }
+    }
+
+    void printArray() {
+        for (int i = 0; i < arrSize; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    void sortArray() {
+        int i;
+        for (i = 0; i < arrSize; i++) {
+            for (int j = i + 1; j < arrSize; j++) {
+                int tmp = 0;
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+            System.out.print(arr[i] + " ");
+        }
+        for (int k = 0; k < arrSize; k++) {
+            for (int m = k + 1; m < arrSize; m++) {
+                int tmp = 0;
+                if (arr[k] < arr[m]) {
+                    tmp = arr[k];
+                    arr[k] = arr[m];
+                    arr[m] = tmp;
+                }
+            }
+            System.out.print(arr[k] + " ");
+        }
+    }
 }
--- src/com/ua/HomeWork/l10/Student.java
+++ src/com/ua/HomeWork/l10/Student.java
@@ -1,4 +1,19 @@
 package com.ua.HomeWork.l10;
 
 public class Student {
+
+    String name;
+    int age;
+    String groupNumber;
+    int course;
+    int averageScore;
+    String curatorName;
+    String gender;
+
+    void study(){
+        System.out.println("Study");
+
+    }
+
+
 }
