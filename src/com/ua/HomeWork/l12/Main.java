d18bbf75226db673dea97df8b352d242f620976b
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-02-23 19:47:38 +0200

    Homework 12 done

---
+++ src/com/ua/HomeWork/l12/Main.java
@@ -0,0 +1,11 @@
+package com.ua.HomeWork.l12;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ChildInit c1 = new ChildInit();
+        ParentInit p1 = new ParentInit();
+    }
+
+
+}
