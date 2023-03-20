6a3cccf0cddba89d5544f8e58e209d26925041ea
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-03-20 20:14:05 +0200

    Homework 18 done

--- src/com/ua/HomeWork/l18/Lists.java
+++ src/com/ua/HomeWork/l18/Lists.java
@@ -23,5 +23,18 @@ public class Lists {
             listTwo.add(random.nextInt(100));
         }
         //System.out.println(listTwo);
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Alex", 23, "B-212", 2, 95, "Mr. Been", "Male"));
+        students.add(new Student("John", 24, "C-425", 4, 82, "Mrs. Castro", "Male"));
+        students.add(new Student("Marta", 21, "F-310", 3, 75, "Mr. Smith", "Female"));
+        students.add(new Student("Peter", 19, "A-112", 1, 60, "Mr. Stark", "Male"));
+        students.add(new Student("Casandra", 25, "F-310", 3, 100, "Mr. Smith", "Female"));
+
+        System.out.print(students + "");
+
+
     }
+
+
 }
--- src/com/ua/HomeWork/l18/Student.java
+++ src/com/ua/HomeWork/l18/Student.java
@@ -10,59 +10,27 @@ public class Student {
         String curatorName;
         String gender;
 
-        public String getName() {
-                return name;
-        }
-
-        public void setName(String name) {
+        public Student(String name, int age, String groupNumber, int course, int averageScore, String curatorName, String gender) {
                 this.name = name;
-        }
-
-        public int getAge() {
-                return age;
-        }
-
-        public void setAge(int age) {
                 this.age = age;
-        }
-
-        public String getGroupNumber() {
-                return groupNumber;
-        }
-
-        public void setGroupNumber(String groupNumber) {
                 this.groupNumber = groupNumber;
-        }
-
-        public int getCourse() {
-                return course;
-        }
-
-        public void setCourse(int course) {
                 this.course = course;
-        }
-
-        public int getAverageScore() {
-                return averageScore;
-        }
-
-        public void setAverageScore(int averageScore) {
                 this.averageScore = averageScore;
-        }
-
-        public String getCuratorName() {
-                return curatorName;
-        }
-
-        public void setCuratorName(String curatorName) {
                 this.curatorName = curatorName;
-        }
-
-        public String getGender() {
-                return gender;
-        }
-
-        public void setGender(String gender) {
                 this.gender = gender;
         }
+
+        @Override
+        public String toString() {
+                return "Student{" +
+                        "name='" + name + '\'' +
+                        ", age=" + age +
+                        ", groupNumber='" + groupNumber + '\'' +
+                        ", course=" + course +
+                        ", averageScore=" + averageScore +
+                        ", curatorName='" + curatorName + '\'' +
+                        ", gender='" + gender + '\'' +
+                        '}';
+        }
 }
+
