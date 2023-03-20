66d82aabf32e5a830c43ba96496fada1dfa1b71c
Oleksii Sachivko <oleksii.sachivko@icloud.com>
2023-03-20 21:06:18 +0200

    Homework 19 done

---
+++ src/com/ua/HomeWork/l19/Main.java
@@ -0,0 +1,14 @@
+package com.ua.HomeWork.l19;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int counter = 1000;
+        int x;
+        for (i = 0; i < counter; i++) {
+
+
+
+        }
+    }
+}
